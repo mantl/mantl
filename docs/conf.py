d6bc23589ec4797b0c0454b84c56564d87e573a9
Brian Hicks <brian@brianthicks.com>
2015-02-05 07:49:56 -0600

    docs: correct author name

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Microservices Infrastructure'
-copyright = u'2015, ???'
+copyright = u'2015, Cisco Systems, Incorporated'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -209,7 +209,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   ('index', 'MicroservicesInfrastructure.tex', u'Microservices Infrastructure Documentation',
-   u'???', 'manual'),
+   u'Cisco Systems, Incorporated', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -239,7 +239,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'microservicesinfrastructure', u'Microservices Infrastructure Documentation',
-     [u'???'], 1)
+     [u'Cisco Systems, Incorporated'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -253,7 +253,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   ('index', 'MicroservicesInfrastructure', u'Microservices Infrastructure Documentation',
-   u'???', 'MicroservicesInfrastructure', 'One line description of project.',
+   u'Cisco Systems, Incorporated', 'MicroservicesInfrastructure', 'One line description of project.',
    'Miscellaneous'),
 ]
 
