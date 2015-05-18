fc681579eb6197fd7db37c3efcb64cb4d40ed326
Chris Aubuchon <Chris.Aubuchon@gmail.com>
2015-05-18 13:09:55 -0500

    Sample single datacenter terraform configuration

---
+++ terraform/openstack.sample.tf
@@ -0,0 +1,25 @@
+module "dc2-keypair" {
+	source = "./terraform/openstack/keypair"
+	auth_url = ""
+	tenant_id = ""
+	tenant_name = ""
+	region = ""
+	public_key = ""
+	keypair_name = ""
+}
+
+module "dc2-hosts" {
+	source = "./terraform/openstack/hosts"
+	auth_url = ""
+	tenant_id = ""
+	tenant_name = ""
+	region = ""
+	flavor_name = ""
+	net_id = ""
+	image_name = ""
+	keypair_name = "${ module.dc2-keypair.keypair_name }"
+	control_count = "2"
+	resource_count = "3"
+	security_groups = "default"
+}
+
