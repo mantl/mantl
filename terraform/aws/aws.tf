d8c2342866224a3d79f4bd56f26eb9d7bf8fa6bf
ryane <ryanesc@gmail.com>
2015-05-21 16:23:35 -0400

    create and route through an internet gateway
    1. This is needed to enable internet access to the control node
    
    2. Terraform does not have the ability to modify the default route table
    created by AWS. The workaround is to create a new route table and then
    associate it with the vpc as the main route table. This leaves an unused
    route table artifact in AWS but it does get cleaned up properly on a
    `terraform destroy`. See
    https://github.com/hashicorp/terraform/issues/748 and
    https://www.terraform.io/docs/providers/aws/r/main_route_table_assoc.html

--- terraform/aws/aws.tf
+++ terraform/aws/aws.tf
@@ -27,6 +27,24 @@ resource "aws_subnet" "main" {
     cidr_block = "${var.network_subnet_ip4}"
 }
 
+resource "aws_internet_gateway" "main" {
+  vpc_id = "${aws_vpc.main.id}"
+}
+
+resource "aws_route_table" "main" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.main.id}"
+  }
+}
+
+resource "aws_main_route_table_association" "main" {
+  vpc_id = "${aws_vpc.main.id}"
+  route_table_id = "${aws_route_table.main.id}"
+}
+
 resource "aws_instance" "mi-control-nodes" {
     ami = "${var.source_ami}"
     instance_type = "${var.control_type}"
