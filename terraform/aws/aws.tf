87ed942df89ef0e68a219c279b410d0edac92011
ryane <ryanesc@gmail.com>
2015-05-21 19:12:42 -0400

    add public ip to workers and refactor sec groups
    - Since we rely on ansible to provision nodes (for now), we need to make
      worker nodes publicly addressable so they can be reached over ssh
        - we should think about better solutions in the future
    
    - I removed some security groups that appeared to be unused:
        - allow_all_icmp
        - allow_all_ssh
        - allow_all_udp_within_subnet
        - allow_all_tcp_within_subnet
    
    - Removed RDP ingress rule
    
    - Split worker and control security groups
        - We probably need to double check that the rules are correct

--- terraform/aws/aws.tf
+++ terraform/aws/aws.tf
@@ -42,7 +42,7 @@ resource "aws_instance" "mi-control-nodes" {
   availability_zone = "${var.availability_zone}"
   instance_type = "${var.control_type}"
   count = "${var.control_count}"
-  vpc_security_group_ids = ["${aws_security_group.external.id}",
+  vpc_security_group_ids = ["${aws_security_group.control.id}",
     "${aws_vpc.main.default_security_group_id}"]
 
   key_name = "${aws_key_pair.deployer.key_name}"
@@ -62,11 +62,13 @@ resource "aws_instance" "mi-worker-nodes" {
   instance_type = "${var.worker_type}"
   count = "${var.worker_count}"
 
-  vpc_security_group_ids = ["${aws_security_group.external.id}",
+  vpc_security_group_ids = ["${aws_security_group.worker.id}",
     "${aws_vpc.main.default_security_group_id}"]
 
 
   key_name = "${aws_key_pair.deployer.key_name}"
+
+  associate_public_ip_address=true
 
   subnet_id = "${aws_subnet.main.id}"
 
@@ -75,132 +77,87 @@ resource "aws_instance" "mi-worker-nodes" {
   }
 }
 
-resource "aws_security_group" "allow_all_icmp" {
-  name = "allow_all_icmp_${var.cluster_id}"
-  description = "Allow all icmp traffic"
+resource "aws_security_group" "control" {
+  name = "control_group_${var.cluster_id}"
+  description = "Allow inbound traffic for control nodes"
+  vpc_id="${aws_vpc.main.id}"
 
-  ingress {
-    from_port = -1
-    to_port = -1
-    protocol = "icmp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags {
-    Name = "allow_icmp"
-  }
-}
-
-resource "aws_security_group" "allow_all_ssh" {
-  name = "allow_all_ssh_${var.cluster_id}"
-  description = "Allow ssh traffic"
-
-  ingress {
+  ingress { # SSH
     from_port = 22
     to_port = 22
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags {
-    Name = "allow_ssh"
-  }
-}
-
-resource "aws_security_group" "allow_all_tcp_within_subnet" {
-  name = "allow_all_tcp_within_subnet_${var.cluster_id}"
-  description = "Allow all tcp traffic within our subnet"
-
-  vpc_id="${aws_vpc.main.id}"
-
-  ingress {
-    from_port = 0
-    to_port = 65535
+  ingress { # Mesos
+    from_port = 5050
+    to_port = 5050
     protocol = "tcp"
-    cidr_blocks = ["${var.network_subnet_ip4}"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags {
-    Name = "allow_all_tcp"
-  }
-}
-
-resource "aws_security_group" "allow_all_udp_within_subnet" {
-  name = "allow_all_udp_within_subnet_${var.cluster_id}"
-  description = "Allow all traffic within our subnet"
-
-  vpc_id="${aws_vpc.main.id}"
-
-  ingress {
-    from_port = 0
-    to_port = 65535
-    protocol = "udp"
-    cidr_blocks = ["${var.network_subnet_ip4}"]
+  ingress { # Marathon
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags {
-    Name = "allow_all_upp"
+  ingress { # ICMP
+    from_port=-1
+    to_port=-1
+    protocol = "icmp"
+    cidr_blocks=["0.0.0.0/0"]
   }
-}
-
-
-resource "aws_security_group" "external" {
-  name = "master_group_${var.cluster_id}"
-  description = "Allow all inbound traffic"
-
-  vpc_id="${aws_vpc.main.id}"
 
   tags {
     Name = "${var.cluster_id}_terraform"
   }
 }
 
-resource "aws_security_group_rule" "allow_ssh" {
-  type = "ingress"
-  from_port = 22
-  to_port = 22
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
+resource "aws_security_group" "worker" {
+  name = "worker_group_${var.cluster_id}"
+  description = "Allow inbound traffic for worker nodes"
+  vpc_id="${aws_vpc.main.id}"
 
-  security_group_id = "${aws_security_group.external.id}"
-}
-resource "aws_security_group_rule" "allow_rdp" {
-  type = "ingress"
-  from_port = 3389
-  to_port = 3389
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
+  ingress { # SSH
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-  security_group_id = "${aws_security_group.external.id}"
-}
-resource "aws_security_group_rule" "allow_mesos" {
-  type = "ingress"
-  from_port = 5050
-  to_port = 5050
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
+  ingress { # Mesos
+    from_port = 5050
+    to_port = 5050
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-  security_group_id = "${aws_security_group.external.id}"
-}
+  ingress { # Marathon
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-resource "aws_security_group_rule" "allow_marathon" {
-  type = "ingress"
-  from_port = 8080
-  to_port = 8080
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
+  ingress { # Consul
+    from_port = 8500
+    to_port = 8500
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-  security_group_id = "${aws_security_group.external.id}"
-}
+  ingress { # ICMP
+    from_port=-1
+    to_port=-1
+    protocol = "icmp"
+    cidr_blocks=["0.0.0.0/0"]
+  }
 
-resource "aws_security_group_rule" "allow_icmp" {
-  type = "ingress"
-  from_port=-1
-  to_port=-1
-  protocol = "icmp"
-  cidr_blocks=["0.0.0.0/0"]
-
-  security_group_id = "${aws_security_group.external.id}"
+  tags {
+    Name = "${var.cluster_id}_terraform"
+  }
 }
 
 resource "aws_key_pair" "deployer" {
