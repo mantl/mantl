67925af2f173e515d4eabf6172477a82be67aada
Brian Hicks <brian@brianthicks.com>
2015-05-22 14:30:54 -0500

    terraform: add more vars to the OpenStack parser

--- plugins/inventory/terraform.py
+++ plugins/inventory/terraform.py
@@ -53,24 +53,30 @@ def parses(prefix):
 
 def calculate_mi_vars(func):
     """calculate microservices-infrastructure vars"""
+
     @wraps(func)
     def inner(*args, **kwargs):
         name, attrs, groups = func(*args, **kwargs)
 
+        # attrs
         if attrs['role'] == 'control':
             attrs['consul_is_server'] = True
         elif attrs['role'] == 'worker':
             attrs['consul_is_server'] = False
+
+        # groups
+        if attrs.get('publicly_routable', False):
+            groups.append('publicly_routable')
 
         return name, attrs, groups
 
     return inner
 
 
-def _parse_prefix(source, prefix):
+def _parse_prefix(source, prefix, sep='.'):
     for compkey, value in source.items():
         try:
-            curprefix, rest = compkey.split('.', 1)
+            curprefix, rest = compkey.split(sep, 1)
         except ValueError:
             continue
 
@@ -80,50 +86,78 @@ def _parse_prefix(source, prefix):
         yield rest, value
 
 
-def parse_attr_list(source, prefix):
-    size_key = '%s.#' % prefix
+def parse_attr_list(source, prefix, sep='.'):
+    size_key = '%s%s#' % (prefix, sep)
     try:
         size = int(source[size_key])
     except KeyError:
         return []
 
     attrs = [{} for _ in range(size)]
-    for compkey, value in _parse_prefix(source, prefix):
-        nth, key = compkey.split('.', 1)
+    for compkey, value in _parse_prefix(source, prefix, sep):
+        nth, key = compkey.split(sep, 1)
         attrs[int(nth)][key] = value
 
     return attrs
 
 
-def parse_dict(source, prefix):
-    return dict(_parse_prefix(source, prefix))
+def parse_dict(source, prefix, sep='.'):
+    return dict(_parse_prefix(source, prefix, sep))
 
 
-def parse_list(source, prefix):
-    return [value for _, value in _parse_prefix(source, prefix)]
+def parse_list(source, prefix, sep='.'):
+    return [value for _, value in _parse_prefix(source, prefix, sep)]
+
 
 @parses('openstack_compute_instance_v2')
+@calculate_mi_vars
 def openstack_host(resource, tfvars=None):
-    raw_attr = resource['primary']['attributes']
-    name = raw_attr['name']
+    raw_attrs = resource['primary']['attributes']
+    name = raw_attrs['name']
     groups = []
 
-    attrs = {}
-
-    attrs.update({
-        'consul_dc': raw_attr['region']
-    })
+    attrs = {
+        'access_ip_v4': raw_attrs['access_ip_v4'],
+        'access_ip_v6': raw_attrs['access_ip_v6'],
+        'flavor': parse_dict(raw_attrs, 'flavor',
+                             sep='_'),
+        'id': raw_attrs['id'],
+        'image': parse_dict(raw_attrs, 'image',
+                            sep='_'),
+        'key_pair': raw_attrs['key_pair'],
+        'metadata': parse_dict(raw_attrs, 'metadata'),
+        'network': parse_attr_list(raw_attrs, 'network'),
+        'region': raw_attrs['region'],
+        'security_groups': parse_list(raw_attrs, 'security_groups'),
+        #ansible
+        'ansible_ssh_port': 22,
+        'ansible_ssh_user': 'centos',
+    }
 
     try:
         attrs.update({
-            'ansible_ssh_host': raw_attr['access_ip_v4'],
+            'ansible_ssh_host': raw_attrs['access_ip_v4'],
+            'publicly_routable': True,
         })
     except (KeyError, ValueError):
-        attrs.update({
-            'ansible_ssh_host': '',
-        })
+        attrs.update({'ansible_ssh_host': '', 'publicly_routable': False, })
 
-    groups.append('role=%s' % raw_attr['metadata.role'])
+    # attrs specific to microservices-infrastructure
+    attrs.update({
+        'consul_dc': attrs['metadata'].get('dc', attrs['region']),
+        'role': attrs['metadata'].get('role', 'none')
+    })
+
+    # add groups based on attrs
+    groups.append('os_image=' + attrs['image']['name'])
+    groups.append('os_flavor=' + attrs['flavor']['name'])
+    groups.extend('os_metadata_%s=%s' % item
+                  for item in attrs['metadata'].items())
+    groups.append('os_region=' + attrs['region'])
+
+    # groups specific to microservices-infrastructure
+    groups.append('role=' + attrs['metadata'].get('role', 'none'))
+    groups.append('dc=' + attrs['consul_dc'])
 
     return name, attrs, groups
 
@@ -173,10 +207,7 @@ def gce_host(resource, tfvars=None):
             'publicly_routable': True,
         })
     except (KeyError, ValueError):
-        attrs.update({
-            'ansible_ssh_host': '',
-            'publicly_routable': False,
-        })
+        attrs.update({'ansible_ssh_host': '', 'publicly_routable': False})
 
     # add groups based on attrs
     groups.extend('gce_image=' + disk['image'] for disk in attrs['disks'])
@@ -193,8 +224,7 @@ def gce_host(resource, tfvars=None):
         groups.append('gce_publicly_routable')
 
     # groups specific to microservices-infrastructure
-    if 'role' in attrs['metadata']:
-        groups.append('role=' + attrs['metadata']['role'])
+    groups.append('role=' + attrs['metadata'].get('role', 'none'))
     groups.append('dc=' + attrs['consul_dc'])
 
     return name, attrs, groups
@@ -214,7 +244,7 @@ def query_list(hosts):
     meta = {}
 
     for name, attrs, hostgroups in hosts:
-        for group in hostgroups:
+        for group in set(hostgroups):
             groups[group].setdefault('hosts', [])
             groups[group]['hosts'].append(name)
 
@@ -228,7 +258,9 @@ def main():
 
     parser = argparse.ArgumentParser(__file__, __doc__)
     modes = parser.add_mutually_exclusive_group(required=True)
-    modes.add_argument('--list', action='store_true', help='list all variables')
+    modes.add_argument('--list',
+                       action='store_true',
+                       help='list all variables')
     modes.add_argument('--host', help='list variables for a single host')
     parser.add_argument('--pretty',
                         action='store_true',
