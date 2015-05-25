15f3904e0c67b35f6e2019864f405b8a445e653b
Florian Grundig <flg@trifork.com>
2015-05-25 19:46:24 +0200

    Refactored tests for ansible inventory plugin

--- test/inventory/test_terraform.py
+++ test/inventory/test_terraform.py
@@ -2,7 +2,8 @@ import terraform
 import unittest
 
 
-class Extension(object):
+class UnittestMixin(object):
+
     def assertDictContainsSubsetDeeply(self, expected, actual, msg=None):
         """Compares nested dictionary structures"""
         for key, value in expected.iteritems():
@@ -16,7 +17,52 @@ class Extension(object):
                 self.assertEqual(expected[key], actual[key], msg)
 
 
-class TestStringMethods(Extension, unittest.TestCase):
+class TestStringMethods(UnittestMixin, unittest.TestCase):
+
+    expected_unified_data = {
+        "_meta": {
+            "hostvars": {
+                "mi-worker-001": {
+                    "ansible_ssh_host": "130.211.119.96",
+                    "ansible_ssh_user": "deploy",
+                    "ansible_ssh_port": 22,
+                    "consul_dc": "dc1",
+                    "metadata": {
+                        "dc": "dc1",
+                        "role": "worker"
+                    }
+                },
+                "mi-control-01": {
+                    "ansible_ssh_host": "104.197.77.162",
+                    "ansible_ssh_user": "deploy",
+                    "ansible_ssh_port": 22,
+                    "consul_dc": "dc1",
+                    "metadata": {
+                        "dc": "dc1",
+                        "role": "control"
+                    }
+                }
+
+            }
+        },
+        "dc=dc1": {
+            "hosts": [
+                "mi-worker-001",
+                "mi-control-01"
+            ]
+        },
+        "role=worker": {
+            "hosts": [
+                "mi-worker-001"
+            ]
+        },
+        "role=control": {
+            "hosts": [
+                "mi-control-01"
+            ]
+        },
+    }
+
     def load_tfstate(self, mock_filenames):
         return terraform.iterresources(mock_filenames)
 
@@ -26,100 +72,9 @@ class TestStringMethods(Extension, unittest.TestCase):
         return terraform.query_list(hosts)
 
     def test_gce_ansible_vars(self):
-        expected = {
-            "_meta": {
-                "hostvars": {
-                    "mi-worker-001": {
-                        "ansible_ssh_host": "130.211.119.96",
-                        "ansible_ssh_user": "deploy",
-                        "ansible_ssh_port": 22,
-                        "consul_dc": "dc1",
-                        "metadata": {
-                            "dc": "dc1",
-                            "role": "worker",
-                            "sshKeys": "myusername:mykey myusername"
-                        }
-                    },
-                    "mi-control-01": {
-                        "ansible_ssh_host": "104.197.77.162",
-                        "ansible_ssh_user": "deploy",
-                        "ansible_ssh_port": 22,
-                        "consul_dc": "dc1",
-                        "metadata": {
-                            "dc": "dc1",
-                            "role": "control",
-                            "sshKeys": "myusername:mykey myusername"
-                        }
-                    }
-
-                }
-            },
-            "dc=dc1": {
-                "hosts": [
-                    "mi-worker-001",
-                    "mi-control-01"
-                ]
-            },
-            "role=worker": {
-                "hosts": [
-                    "mi-worker-001"
-                ]
-            },
-            "role=control": {
-                "hosts": [
-                    "mi-control-01"
-                ]
-            },
-        }
-
         parsed = self.parse(['inventory/gce.state.snapshot'])
-
-        self.assertDictContainsSubsetDeeply(expected, parsed)
+        self.assertDictContainsSubsetDeeply(self.expected_unified_data, parsed)
 
     def test_aws_ansible_vars(self):
-        expected = {
-            "_meta": {
-                "hostvars": {
-                    "mi-worker-001": {
-                        "ansible_ssh_host": "130.211.119.96",
-                        "ansible_ssh_user": "deploy",
-                        "ansible_ssh_port": 22,
-                        "consul_dc": "dc1",
-                        "metadata": {
-                            "dc": "dc1",
-                            "role": "worker",
-                        }
-                    },
-                    "mi-control-01": {
-                        "ansible_ssh_host": "104.197.77.162",
-                        "ansible_ssh_user": "deploy",
-                        "ansible_ssh_port": 22,
-                        "consul_dc": "dc1",
-                        "metadata": {
-                            "dc": "dc1",
-                            "role": "control",
-                        }
-                    }
-                }
-            },
-            "dc=dc1": {
-                "hosts": [
-                    "mi-worker-001",
-                    "mi-control-01"
-                ]
-            },
-            "role=worker": {
-                "hosts": [
-                    "mi-worker-001"
-                ]
-            },
-            "role=control": {
-                "hosts": [
-                    "mi-control-01"
-                ]
-            },
-        }
-
         parsed = self.parse(['inventory/aws.state.snapshot'])
-
-        self.assertDictContainsSubsetDeeply(expected, parsed)
+        self.assertDictContainsSubsetDeeply(self.expected_unified_data, parsed)
