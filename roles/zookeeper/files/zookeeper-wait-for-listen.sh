eee01ed3c34b0550414b56170faed56e803fa74b
Chris Aubuchon <Chris.Aubuchon@gmail.com>
2015-06-01 14:12:23 -0500

    Add command line argument for hostname to zookeeper-wait-for-listen.sh When ansible runs in vagrant the $HOSTNAME variable has localhost.localdomain

--- roles/zookeeper/files/zookeeper-wait-for-listen.sh
+++ roles/zookeeper/files/zookeeper-wait-for-listen.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+[[ -n "$1" ]] && host=$1 || host=$HOSTNAME
+
 MAX_SECONDS=60
 while /bin/true
 do
-    [[ "$(echo ruok | nc "$HOSTNAME" 2181 2>/dev/null)" == "imok" ]] && exit 0 || sleep 1
+    [[ "$(echo ruok | nc "$host" 2181 2>/dev/null)" == "imok" ]] && exit 0 || sleep 1
     [[ "$SECONDS" -ge "$MAX_SECONDS" ]] && exit 1
 done
 
