7b208f75567966a8cc9af238087744252530403a
ryane <ryanesc@gmail.com>
2015-06-10 11:27:48 -0400

    move wait-for-hosts from docker-launch into playbook

--- docker_launch.sh
+++ docker_launch.sh
@@ -5,20 +5,7 @@ if [ ! -f ./security.yml ]; then
     ./security-setup --enable=false
 fi
 
-cat > wait-for-hosts.yml << EOF
-- hosts: all
-  gather_facts: no
-  tasks:
-
-    - name: wait for ssh to become available
-      local_action: wait_for
-                    port=22
-                    host="{{ ansible_ssh_host | default(inventory_hostname) }}"
-                    search_regex=OpenSSH
-                    delay=10
-EOF
-
 terraform get
 terraform apply -state=$TERRAFORM_STATE_ROOT/terraform.tfstate
-ansible-playbook wait-for-hosts.yml
-ansible-playbook terraform.yml --extra-vars=@security.yml
+ansible-playbook /mi/playbooks/wait-for-hosts.yml
+ansible-playbook /mi/terraform.yml --extra-vars=@security.yml
