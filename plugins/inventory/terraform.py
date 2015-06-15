eb5d56d3daf2343a63be96e11f41b487b6d9e507
Brian Hicks <brian@brianthicks.com>
2015-06-15 09:49:33 -0500

    terraform: add missing comma

--- plugins/inventory/terraform.py
+++ plugins/inventory/terraform.py
@@ -214,7 +214,7 @@ def openstack_host(resource, module_name):
         'security_groups': parse_list(raw_attrs, 'security_groups'),
         # ansible
         'ansible_ssh_port': 22,
-        'ansible_ssh_user': raw_attrs.get('metadata.ssh_user', 'centos')
+        'ansible_ssh_user': raw_attrs.get('metadata.ssh_user', 'centos'),
         # workaround for an OpenStack bug where hosts have a different domain
         # after they're restarted
         'host_domain': 'novalocal',
