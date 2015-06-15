49798998483e9b801914abda37799af2c5476dae
Brian Hicks <brian@brianthicks.com>
2015-06-15 12:25:55 -0500

    terraform(do): add datacenter

--- terraform/digitalocean/hosts/main.tf
+++ terraform/digitalocean/hosts/main.tf
@@ -1,12 +1,13 @@
 # input variables
-variable short_name { default = "mi" }
+variable control_count { default = 1 }
+variable control_size { default = "1gb" }
+variable datacenter { default = "mi" }
 variable image_name { default = "centos-7-0-x64" }
 variable region_name { default = "nyc3" }
-variable control_count { default = 1 }
-variable worker_count { default = 3 }
-variable control_size { default = "1gb" }
-variable worker_size { default = "4gb" }
+variable short_name { default = "mi" }
 variable ssh_key { }
+variable worker_count { default = 3 }
+variable worker_size { default = "4gb" }
 
 # create resources
 resource "digitalocean_droplet" "control" {
@@ -16,7 +17,7 @@ resource "digitalocean_droplet" "control" {
   region = "${var.region_name}"
   size = "${var.control_size}"
   ssh_keys = ["${var.ssh_key}"]
-  user_data = "{\"role\":\"control\"}"
+  user_data = "{\"role\":\"control\",\"dc\":\"${var.datacenter}\"}"
 }
 
 resource "digitalocean_droplet" "worker" {
@@ -26,5 +27,5 @@ resource "digitalocean_droplet" "worker" {
   region = "${var.region_name}"
   size = "${var.worker_size}"
   ssh_keys = ["${var.ssh_key}"]
-  user_data = "{\"role\":\"worker\"}"
+  user_data = "{\"role\":\"worker\",\"dc\":\"${var.datacenter}\"}"
 }
