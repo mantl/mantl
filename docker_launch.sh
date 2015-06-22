f567825d89aa21dcaecded8a7061dc3a53ba698a
ryane <ryanesc@gmail.com>
2015-06-22 10:52:09 -0400

    dockerfile ssh: support passphrase and any key name

--- docker_launch.sh
+++ docker_launch.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 set -e
 
+SSH_KEY=${SSH_KEY:-"/root/.ssh/id_rsa"}
+
+eval `ssh-agent -s` && ssh-add $SSH_KEY
+
 if [ ! -f ./security.yml ]; then
     ./security-setup --enable=false
 fi
 
 terraform get
 terraform apply -state=$TERRAFORM_STATE_ROOT/terraform.tfstate
-ansible-playbook /mi/playbooks/wait-for-hosts.yml
-ansible-playbook /mi/terraform.yml --extra-vars=@security.yml
+ansible-playbook /mi/playbooks/wait-for-hosts.yml --private-key $SSH_KEY
+ansible-playbook /mi/terraform.yml --extra-vars=@security.yml --private-key $SSH_KEY
