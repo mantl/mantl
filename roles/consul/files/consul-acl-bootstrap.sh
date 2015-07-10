05f0eab96bf0ee05b1b69a2afa6f49885413e417
Chris Aubuchon <Chris.Aubuchon@gmail.com>
2015-07-10 13:44:02 -0500

    Integrate consul-utils into the build.
    Replace curls to Consul with calls to `consul-cli kv-write`

--- roles/consul/files/consul-acl-bootstrap.sh
+++ roles/consul/files/consul-acl-bootstrap.sh
@@ -5,5 +5,7 @@ set -e
 MASTER_TOKEN="$1"
 AGENT_TOKEN="$2"
 
-curl -X PUT "http://localhost:8500/v1/acl/create?token=$MASTER_TOKEN" \
-    -d "{ \"ID\": \"$AGENT_TOKEN\", \"Name\": \"agent_policy\", \"Type\": \"client\", \"Rules\": \"service \\\"\\\" { policy = \\\"write\\\"}\"}"
+consul-cli acl-update --token=${MASTER_TOKEN} --name=agent_policy \
+    --rule='key::write' \
+    --rule='service::write' \
+    ${AGENT_TOKEN}
