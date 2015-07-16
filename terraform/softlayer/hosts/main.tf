2018cde3a8b867e34f8398578f6f9687240fc7f0
eirslett <eirik.sletteberg@finn.no>
2015-07-16 15:11:45 +0200

    Add support for SoftLayer
    This adds .tf files for SoftLayer virtual guests,
    and a simple .tf file for using them.
    The change also depends on terraform.py being updated
    so that it can read the inventory list from softlayer.tfstate.

---
+++ terraform/softlayer/hosts/main.tf
@@ -0,0 +1,36 @@
+# input variables
+variable control_count { default = 1 }
+variable control_size { default = 4096 }
+variable datacenter { default = "mi" }
+variable domain { default = "example.com" }
+variable image_name { default = "CENTOS_7_64" }
+variable region_name { default = "ams01" }
+variable short_name { default = "mi" }
+variable ssh_key { }
+variable worker_count { default = 3 }
+variable worker_size { default = 4096 }
+
+# create resources
+resource "softlayer_virtualserver" "control" {
+  count = "${var.control_count}"
+  name = "${var.short_name}-control-${format("%02d", count.index+1)}"
+  domain = "${var.domain}"
+  image = "${var.image_name}"
+  region = "${var.region_name}"
+  ram = "${var.control_size}"
+  cpu = 1
+  ssh_keys = ["${var.ssh_key}"]
+  user_data = "{\"role\":\"control\",\"dc\":\"${var.datacenter}\"}"
+}
+
+resource "softlayer_virtualserver" "worker" {
+  count = "${var.worker_count}"
+  name = "${var.short_name}-worker-${format("%03d", count.index+1)}"
+  domain = "${var.domain}"
+  image = "${var.image_name}"
+  region = "${var.region_name}"
+  ram = "${var.worker_size}"
+  cpu = 1
+  ssh_keys = ["${var.ssh_key}"]
+  user_data = "{\"role\":\"worker\",\"dc\":\"${var.datacenter}\"}"
+}
