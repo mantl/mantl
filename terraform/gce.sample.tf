73023c85bf73cb7d05aedb243ef2da9cd8d54b59
Brian Hicks <brian@brianthicks.com>
2015-11-19 14:36:14 -0600

    terraform: remove DNS examples from sample configs

--- terraform/gce.sample.tf
+++ terraform/gce.sample.tf
@@ -18,15 +18,3 @@ module "gce-dc" {
   worker_count = 3
   edge_count = 2
 }
-
-# Example setup for DNS:
-# module "dnsimple-dns" { # This could also be "google-cloud-dns"
-#   source = "./terraform/dnsimple/dns" # This could also be "./terraform/gce/dns"
-#   short_name = "mi"
-#   control_count = 3
-#   worker_count = 3
-#   domain = "example.com"
-#   control_ips = "${module.gce-dc.control_ips}"
-#   worker_ips = "${module.gce-dc.worker_ips}"
-#   # managed_zone = "my-managed-zone" # would be required for Google cloud DNS
-# }
--- terraform/softlayer.sample.tf
+++ terraform/softlayer.sample.tf
@@ -16,15 +16,3 @@ module "softlayer-hosts" {
   worker_count = 3
   edge_count = 2
 }
-
-# Example setup for DNS:
-# module "dnsimple-dns" { # This could also be "google-cloud-dns"
-#   source = "./terraform/dnsimple/dns" # This could also be "./terraform/gce/dns"
-#   short_name = "mi"
-#   control_count = 3
-#   worker_count = 3
-#   domain = "example.com"
-#   control_ips = "${module.softlayer-hosts.control_ips}"
-#   worker_ips = "${module.softlayer-hosts.worker_ips}"
-#   # managed_zone = "my-managed-zone" # this would be required for Google cloud DNS
-# }
