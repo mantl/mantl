042d7b41102ba3d030b68f7c72d3b7bb5d9b3b7b
Chris Aubuchon <Chris.Aubuchon@gmail.com>
2015-12-15 16:03:42 -0600

    Add acl for /v1/kv/core for vault

--- roles/consul/files/consul-acl-bootstrap.sh
+++ roles/consul/files/consul-acl-bootstrap.sh
@@ -10,5 +10,6 @@ consul-cli acl-update --token=${MASTER_TOKEN} --name=agent_policy \
     --rule='key:marathon:deny' \
     --rule='key:vault:deny' \
     --rule='key:secure:deny' \
+    --rule='key:core:deny' \
     --rule='service::write' \
     ${AGENT_TOKEN}
