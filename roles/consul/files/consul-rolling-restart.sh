d3857df46202b4be5c1d867f58008151612602b0
Chris Aubuchon <Chris.Aubuchon@gmail.com>
2016-02-02 14:29:53 -0600

    Add token to consul scripts

--- roles/consul/files/consul-rolling-restart.sh
+++ roles/consul/files/consul-rolling-restart.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-consul lock -n=1 locks/consul "/bin/bash -c \" 
+set -x 
+
+token=$1
+
+if [ -n "${token}" ]; then
+  cargs="-token=${token}"
+fi
+
+consul lock ${cargs} -n=1 locks/consul "/bin/bash -x -c \" 
     sleep 5; 
-    /usr/local/bin/consul-wait-for-leader.sh || exit 1; 
-    bash -c 'sleep 2; systemctl restart consul' & 
+    /usr/local/bin/consul-wait-for-leader.sh ${token} || exit 1; 
+    bash -c 'sleep 2 && systemctl restart consul' & 
 \""
+
+# Give 'sleep 2 && systemctl restart consul' time to execute before exiting
+sleep 5
 
 exit 0
