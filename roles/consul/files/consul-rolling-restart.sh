6d750ac3e1b6e8a91802bd7f7dc3768a333d7015
Chris Aubuchon <Chris.Aubuchon@gmail.com>
2016-02-03 17:55:45 -0600

    Re-improve logic in consul-rolling-restart

--- roles/consul/files/consul-rolling-restart.sh
+++ roles/consul/files/consul-rolling-restart.sh
@@ -5,16 +5,32 @@ set -x
 token=$1
 
 if [ -n "${token}" ]; then
-  cargs="-token=${token}"
+  ccargs="--token=${token}"
 fi
 
-consul lock ${cargs} -n=1 locks/consul "/bin/bash -x -c \" 
-    sleep 5; 
-    /usr/local/bin/consul-wait-for-leader.sh ${token} || exit 1; 
-    bash -c 'sleep 2 && systemctl restart consul' & 
-\""
+# Non-server nodes can be restart immediately with no effect on the quorum
+#
+if [ $(consul-cli agent-self ${ccargs} | jq -r .Member.Tags.role) == node ]; then
+  systemctl restart consul
 
-# Give 'sleep 2 && systemctl restart consul' time to execute before exiting
+  exit 0
+fi
+
+# Try to acquire a lock on 'locks/consul'
+sessionid=$(consul-cli kv-lock ${ccargs} locks/consul)
+
+# Lock acquired. Pause briefly to allow the previous holder to restart
+# If it takes longer than five seconds run `systemctl restart consul`
+# after releasing the lock then we might cause a quorum outage
 sleep 5
+
+# Verify that there is a leader before releasing the lock and restarting
+/usr/local/bin/consul-wait-for-leader.sh
+
+# Release the lock
+consul-cli kv-unlock ${ccargs} locks/consul --session=${sessionid}
+
+# Restart the service
+systemctl restart consul
 
 exit 0
