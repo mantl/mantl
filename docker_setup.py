e5f9bb076095b237e1172e6fd8df69e1162a78af
Sam Hatfield <samuel.e.hatfield@gmail.com>
2016-03-10 11:20:30 -0600

    Add logging, set global docker envs in travis
    When I ported the setup logic to python, I did not adding any logging
    output. I have fixed that, by including helpful messages to users on
    what the setup is currently doing. I also have the setup script set the
    SSH_KEY env if none is set, instead of aborting.
    
    I was forgetting to add settings to multiple places, so I figured that
    it was time for some DRY refactoring. This adds global envs for the
    docker run lines, to make it easier to keep things consistent.
    
    For example, I changed docker run to attach stdout & stderr instead of
    allocating a pseudo-tty. This change allows the security setup script to
    generate credentials randomly, as they did before.
    
    The volume parts of the docker run call need to be placed on the same
    lines, because that's the most direct way to ensure that they are
    populated to the correct values. If there is a DRY way of doing this,
    that would be better, but I can't think of anything right now.
    
    During my heavy edits of the travis file, I found that the indentation
    was inconsistent. I ensured that my editor had the correct tab/space
    settings, and adjusted the indentation.

--- docker_setup.py
+++ docker_setup.py
@@ -12,28 +12,31 @@ from subprocess import call
 
 def link_or_generate_ssh_key():
     if 'SSH_KEY' not in os.environ:
-        print('SSH_KEY not set in environment, exiting')
-        exit(1)
+        os.environ['SSH_KEY'] = 'id_rsa'
 
     ssh_key = join(os.environ['MANTL_CONFIG_DIR'], os.environ['SSH_KEY'])
     if not exists(ssh_key):
         call(split('ssh-keygen -N "" -f {}'.format(ssh_key)))
 
+    print("Symlinking {} to /root/.ssh/id_rsa".format(ssh_key))
     symlink(ssh_key, '/root/.ssh/id_rsa')
 
 
 def link_terraform_files():
-    tfs = [f for f in os.listdir(os.environ['MANTL_CONFIG_DIR'])
+    tfs = [os.path.abspath(f) for f in os.listdir(os.environ['MANTL_CONFIG_DIR'])
             if f.endswith('.tf')]
-    if len(tfs) == 0:
+    if len(tfs):
+        for tf in tfs:
+            print("Symlinking {} to {}".format(tf, os.path.basename(tf)))
+            symlink(tf, os.path.basename(tf))
+    else:
         if 'MANTL_PROVIDER' not in os.environ:
             print("mantl.readthedocs.org for provider")
             exit(1)
         tf = 'terraform/{}.sample.tf'.format(os.environ['MANTL_PROVIDER'])
-        symlink(tf, 'terraform.tf')
-    else:
-        for tf in files:
-            symlink(tf, os.path.basename(tf))
+
+        print("Symlinking {} to mantl.tf".format(tf))
+        symlink(tf, 'mantl.tf')
 
 
 def link_ansible_playbook():
@@ -41,14 +44,17 @@ def link_ansible_playbook():
     if not exists(ansible_playbook):
         ansible_playbook = 'sample.yml'
 
+    print("Symlinking {} to mantl.yml".format(ansible_playbook))
     symlink(ansible_playbook, 'mantl.yml')
 
 
 def link_or_generate_security_file():
     security_file = join(os.environ['MANTL_CONFIG_DIR'], 'security.yml')
     if exists(security_file):
+        print("Symlinking {} to security.yml".format(security_file))
         symlink(security_file, 'security.yml')
     else:
+        print("Generating security.yml via security-setup")
         call(split('./security-setup --enable=false'))
 
 
