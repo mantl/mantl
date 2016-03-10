ad1c52b578b4512cd6846d72a27162aca7209573
Paul Frybarger <pfrybarger@gmail.com>
2016-03-10 15:07:11 -0600

    Multiline comments in openstack-modules.sample.tf

--- terraform/openstack-modules.sample.tf
+++ terraform/openstack-modules.sample.tf
@@ -98,41 +98,49 @@ module "instances-edge" {
 }
 
 # create floating ip for use with control load balancer - uncomment to use
-#resource "openstack_networking_floatingip_v2" "control-lb-floating-ip" {
-#  pool = "${var.floating_ip_pool}"
-#}
+/*
+resource "openstack_networking_floatingip_v2" "control-lb-floating-ip" {
+  pool = "${var.floating_ip_pool}"
+}
+*/
 
 # create neutron lb (via lbaas) for control nodes - uncomment to use
-#module "control-lb" {
-#  source = "./terraform/openstack/lbaas"
-#  name = "${var.name}"
-#  role = "control"
-#  protocol = "HTTPS"
-#  subnet_id = "${module.network.subnet_uuid}"
-#  lb_method = "ROUND_ROBIN"
-#  internal_ips = "${module.instances-control.ip_v4_list}"
-#  lb_port = 443
-#  upstream_port = 443
-#  floating_ip = "${openstack_networking_floatingip_v2.control-lb-floating-ip.address}"
-#  count = "${var.control_count}"
-#}
+/*
+module "control-lb" {
+  source = "./terraform/openstack/lbaas"
+  name = "${var.name}"
+  role = "control"
+  protocol = "HTTPS"
+  subnet_id = "${module.network.subnet_uuid}"
+  lb_method = "ROUND_ROBIN"
+  internal_ips = "${module.instances-control.ip_v4_list}"
+  lb_port = 443
+  upstream_port = 443
+  floating_ip = "${openstack_networking_floatingip_v2.control-lb-floating-ip.address}"
+  count = "${var.control_count}"
+}
+*/
 
 # create floating ip for use with edge load balancer - uncomment to use
-#resource "openstack_networking_floatingip_v2" "edge-lb-floating-ip" {
-#  pool = "${var.floating_ip_pool}"
-#}
+/*
+resource "openstack_networking_floatingip_v2" "edge-lb-floating-ip" {
+  pool = "${var.floating_ip_pool}"
+}
+*/
 
 # create neutron lb (via lbaas) for edge nodes - uncomment to use
-#module "edge-lb" {
-#  source = "./terraform/openstack/lbaas"
-#  name = "${var.name}"
-#  role = "edge"
-#  protocol = "HTTP"
-#  subnet_id = "${module.network.subnet_uuid}"
-#  lb_method = "ROUND_ROBIN"
-#  internal_ips = "${module.instances-edge.ip_v4_list}"
-#  lb_port = 80
-#  upstream_port = 80
-#  floating_ip = "${openstack_networking_floatingip_v2.edge-lb-floating-ip.address}"
-#  count = "${var.edge_count}"
-#}
+/*
+module "edge-lb" {
+  source = "./terraform/openstack/lbaas"
+  name = "${var.name}"
+  role = "edge"
+  protocol = "HTTP"
+  subnet_id = "${module.network.subnet_uuid}"
+  lb_method = "ROUND_ROBIN"
+  internal_ips = "${module.instances-edge.ip_v4_list}"
+  lb_port = 80
+  upstream_port = 80
+  floating_ip = "${openstack_networking_floatingip_v2.edge-lb-floating-ip.address}"
+  count = "${var.edge_count}"
+}
+*/
