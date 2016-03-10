69b1fa54b3859331b87af06e83b5f8dca26e3c8e
Paul Frybarger <pfrybarger@gmail.com>
2016-03-10 14:50:09 -0600

    Add terraform openstack module for creating LB (via Neutron LBaaS).

--- terraform/openstack/network/main.tf
+++ terraform/openstack/network/main.tf
@@ -29,3 +29,7 @@ resource "openstack_networking_router_interface_v2" "router-interface" {
 output "network_uuid" {
   value = "${openstack_networking_network_v2.network.id}"
 }
+
+output "subnet_uuid" {
+  value = "${openstack_networking_subnet_v2.subnet.id}"
+}
