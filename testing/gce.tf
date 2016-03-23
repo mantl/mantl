873bd1c7a7df2d2526c142de0c29bddfe19d4eb5
Sam Hatfield <samuel.e.hatfield@gmail.com>
2016-03-23 15:25:09 -0500

    Update testing/gce.tf file to use modules
    I was comparing the gce terraform file that I made for testing to the
    most recent gce.sample.tf file. It turns out that I never updated the
    file to use the modules! I need to work on getting the testing system to
    test against the sample files instead of custom ones.

--- testing/gce.tf
+++ testing/gce.tf
@@ -1,23 +1,107 @@
 variable "build_number" {}
 
-provider "google" {
-  account_file = ""
-  region = "us-central1"
-  project = "mantl-ci"
+variable "control_count" { default = 3 }
+variable "datacenter" {default = "mantl-dc"}
+variable "edge_count" { default = 1}
+variable "image" {default = "centos-7-v20160119"}
+variable "long_name" {default = "ciscocloud-mantl-ci-${var.build_number}"}
+variable "short_name" {default = "mantl-ci-${var.build_number}"}
+variable "ssh_key" {default = "~/.ssh/id_rsa.pub"}
+variable "ssh_user" {default = "centos"}
+variable "worker_count" {default = 4}
+variable "control_type" { default = "n1-standard-1" }
+variable "edge_type" { default = "n1-standard-1" }
+variable "worker_type" { default = "n1-standard-2" }
+variable "zones" {
+  default = "us-central1-a,us-central1-b"
 }
 
-module "mantl-ci-dc" {
-  source = "./terraform/gce"
-  datacenter = "mantl-dc"
-  control_type = "n1-standard-1"
-  worker_type = "n1-highcpu-2"
-  network_ipv4 = "10.0.0.0/16"
-  long_name = "ciscocloud-mantl-ci-${var.build_number}"
-  short_name = "mantl-ci-${var.build_number}"
+provider "google" {
+  project = "mantl-ci"
   region = "us-central1"
-  zone = "us-central1-a"
-  control_count = 3
-  worker_count = 3
-  edge_count = 2
-  ssh_key = "/root/.ssh/id_rsa.pub"
+}
+
+module "gce-network" {
+  source = "./terraform/gce/network"
+  network_ipv4 = "10.0.0.0/16"
+  long_name = "${var.long_name}"
+  short_name = "${var.short_name}"
+}
+
+# remote state example
+# _local is for development only s3 or something else should be used
+# https://github.com/hashicorp/terraform/blob/master/state/remote/remote.go#L47
+# https://www.terraform.io/docs/state/remote.html
+#resource "terraform_remote_state" "gce-network" {
+#  backend = "_local"
+#  config {
+#    path = "./terraform/gce/network/terraform.tfstate"
+#  }
+#}
+
+
+module "control-nodes" {
+  source = "./terraform/gce/instance"
+  count = "${var.control_count}"
+  datacenter = "${var.datacenter}"
+  image = "${var.image}"
+  machine_type = "${var.control_type}"
+  network_name = "${module.gce-network.network_name}"
+  #network_name = "${terraform_remote_state.gce-network.output.network_name}"
+  role = "control"
+  short_name = "${var.short_name}"
+  ssh_user = "${var.ssh_user}"
+  ssh_key = "${var.ssh_key}"
+  zones = "${var.zones}"
+}
+
+module "edge-nodes" {
+  source = "./terraform/gce/instance"
+  count = "${var.edge_count}"
+  datacenter = "${var.datacenter}"
+  image = "${var.image}"
+  machine_type = "${var.edge_type}"
+  network_name = "${module.gce-network.network_name}"
+  #network_name = "${terraform_remote_state.gce-network.output.network_name}"
+  role = "edge"
+  short_name = "${var.short_name}"
+  ssh_user = "${var.ssh_user}"
+  ssh_key = "${var.ssh_key}"
+  zones = "${var.zones}"
+}
+
+module "worker-nodes" {
+  source = "./terraform/gce/instance"
+  count = "${var.worker_count}"
+  datacenter = "${var.datacenter}"
+  image = "${var.image}"
+  machine_type = "${var.worker_type}"
+  network_name = "${module.gce-network.network_name}"
+  #network_name = "${terraform_remote_state.gce-network.output.network_name}"
+  role = "worker"
+  short_name = "${var.short_name}"
+  ssh_user = "${var.ssh_user}"
+  ssh_key = "${var.ssh_key}"
+  zones = "${var.zones}"
+}
+
+module "network-lb" {
+  source = "./terraform/gce/lb"
+  instances = "${module.edge-nodes.instances}"
+  short_name = "${var.short_name}"
+}
+
+module "cloud-dns" {
+  source = "./terraform/gce/dns"
+  control_count = "${var.control_count}"
+  control_ips = "${module.control-nodes.gce_ips}"
+  domain = "mydomain.com"
+  edge_count = "${var.edge_count}"
+  edge_ips = "${module.edge-nodes.gce_ips}"
+  lb_ip = "${module.network-lb.public_ip}"
+  managed_zone = "my-cloud-dns-zone"
+  short_name = "${var.short_name}"
+  subdomain = ".service"
+  worker_count = "${var.worker_count}"
+  worker_ips = "${module.worker-nodes.gce_ips}"
 }
