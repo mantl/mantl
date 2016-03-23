1ec8aa44dd87028092b4a9667eb1388a89b68cf8
Sam Hatfield <samuel.e.hatfield@gmail.com>
2016-03-23 15:38:49 -0500

    Move string interpolations to modules
    The short/long-name vars in my testing/gce.tf file had string
    interpolations in them, and that is not allowed in terraform. Therefore,
    I have to move them to the module definitions themeselves. The default
    values now assume that the build number is 0-0.

--- testing/gce.tf
+++ testing/gce.tf
@@ -4,8 +4,8 @@ variable "control_count" { default = 3 }
 variable "datacenter" {default = "mantl-dc"}
 variable "edge_count" { default = 1}
 variable "image" {default = "centos-7-v20160119"}
-variable "long_name" {default = "ciscocloud-mantl-ci-${var.build_number}"}
-variable "short_name" {default = "mantl-ci-${var.build_number}"}
+variable "long_name" {default = "ciscocloud-mantl-ci-0-0"}
+variable "short_name" {default = "mantl-ci-0-0"}
 variable "ssh_key" {default = "~/.ssh/id_rsa.pub"}
 variable "ssh_user" {default = "centos"}
 variable "worker_count" {default = 4}
@@ -24,8 +24,8 @@ provider "google" {
 module "gce-network" {
   source = "./terraform/gce/network"
   network_ipv4 = "10.0.0.0/16"
-  long_name = "${var.long_name}"
-  short_name = "${var.short_name}"
+  long_name = "ciscocloud-mantl-ci-${var.build_number}"
+  short_name = "mantl-ci-${var.build_number}"
 }
 
 # remote state example
@@ -49,7 +49,7 @@ module "control-nodes" {
   network_name = "${module.gce-network.network_name}"
   #network_name = "${terraform_remote_state.gce-network.output.network_name}"
   role = "control"
-  short_name = "${var.short_name}"
+  short_name = "mantl-ci-${var.build_number}"
   ssh_user = "${var.ssh_user}"
   ssh_key = "${var.ssh_key}"
   zones = "${var.zones}"
@@ -64,7 +64,7 @@ module "edge-nodes" {
   network_name = "${module.gce-network.network_name}"
   #network_name = "${terraform_remote_state.gce-network.output.network_name}"
   role = "edge"
-  short_name = "${var.short_name}"
+  short_name = "mantl-ci-${var.build_number}"
   ssh_user = "${var.ssh_user}"
   ssh_key = "${var.ssh_key}"
   zones = "${var.zones}"
@@ -79,7 +79,7 @@ module "worker-nodes" {
   network_name = "${module.gce-network.network_name}"
   #network_name = "${terraform_remote_state.gce-network.output.network_name}"
   role = "worker"
-  short_name = "${var.short_name}"
+  short_name = "mantl-ci-${var.build_number}"
   ssh_user = "${var.ssh_user}"
   ssh_key = "${var.ssh_key}"
   zones = "${var.zones}"
@@ -88,7 +88,7 @@ module "worker-nodes" {
 module "network-lb" {
   source = "./terraform/gce/lb"
   instances = "${module.edge-nodes.instances}"
-  short_name = "${var.short_name}"
+  short_name = "mantl-ci-${var.build_number}"
 }
 
 module "cloud-dns" {
@@ -100,7 +100,7 @@ module "cloud-dns" {
   edge_ips = "${module.edge-nodes.gce_ips}"
   lb_ip = "${module.network-lb.public_ip}"
   managed_zone = "my-cloud-dns-zone"
-  short_name = "${var.short_name}"
+  short_name = "mantl-ci-${var.build_number}"
   subdomain = ".service"
   worker_count = "${var.worker_count}"
   worker_ips = "${module.worker-nodes.gce_ips}"
