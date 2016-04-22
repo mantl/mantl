3cd0a059c7e8078ed6ae568e6466eaf0b60855d3
Mike Lundy <mike@fluffypenguin.org>
2016-04-21 17:14:25 -0700

    [packer] EPEL 7.5 is gone, switch to EPEL 7.6
    This also makes the curl in the vagrant provision correctly detect a 404
    and exit, rather than attempting to install an html page

--- packer/scripts/ansible.sh
+++ packer/scripts/ansible.sh
@@ -3,7 +3,7 @@
 update-ca-trust
 
 # Install EPEL repository
-rpm -ivh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+rpm -ivh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-6.noarch.rpm
 
 # Install Ansible
 yum -y install ansible
