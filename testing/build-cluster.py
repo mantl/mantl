5d210febfe3f03ed1dd9b9781062852c5d2f7cd5
Langston Barrett <langston.barrett@gmail.com>
2016-06-24 15:36:35 +0100

    travis: terraform plan CLC and Softlayer (#1532)
    * travis: `terraform plan` all providers
    
    removes some unnecessary vars, MANTL_CI_FORK_CHECK
    and ARE_SECRETS_AVAILABLE, both of which can be
    deduced from DOCKER_SECRETS.
    
    * it's sys.argv, not os.argv
    
    * Avoid KeyError using os.environ.get()
    
    * use versioned docker image with terraform v0.6.16
    
    * use centos with tf 0.6.16
    
    * upgrade terraform to v0.7.0-rc2
    
    * plan: don't expect input or refresh state
    
    * specify options with '='
    
    * only destroy if there are secrets
    
    * create test tf configs for triton, clc, softlayer, vsphere
    
    * add build_number to same tf files
    
    * remove vsphere, add CLC credentials
    
    vsphere needs a valid server, so we can't use it
    we have recurring credits on CLC, let's actually test!
    
    * clc: remove fake credentials
    
    * clc: use shorter `name`
    
    * softlayer: use virtual_guest instead of virtualserver
    
    * softlayer: add hourly_billing and local_disk
    
    * clc: make names shorter
    
    * remove failing clc build
    
    * don't build clc, only plan (but keep secrets)
    
    * fix typo triton.tf -> triton
    
    * don't actually build CLC, just lint it
    
    * fix logic in deciding which kind of build to do
    
    * fix bad merge

--- testing/build-cluster.py
+++ testing/build-cluster.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python2
 from __future__ import print_function
 import subprocess
-from sys import exit
-from os import devnull, environ, symlink
+from sys import exit, argv
 from time import sleep
+import os
+
 
 def run_cmd(cmd, attempts=1):
     """ Runs a command attempts times, logging its output. Returns True if it
     succeeds once, or False if it never does. """
     try:
         for i in range(attempts):
-            proc = subprocess.Popen(cmd, stdin=open(devnull, "r"))
+            proc = subprocess.Popen(cmd, stdin=open(os.devnull, "r"))
             proc.wait()
             if not proc.returncode == 0:
                 print("Command ", str(cmd), " failed")
@@ -22,6 +23,7 @@ def run_cmd(cmd, attempts=1):
     except OSError as e:
         print("Error while attempting to run command ", cmd)
         print(e)
+
 
 def run_cmds(cmds, fail_sequential=False):
     """ Run a list of ([args], tries) tuples, aborting the run if a single one
@@ -48,16 +50,17 @@ if __name__ == "__main__":
     ]
     setup = [
         (["terraform", "get"], 1),
-        (["terraform", "plan"], 1),
-        (["terraform", "apply"], 1),
-        (ap + ["playbooks/wait-for-hosts.yml"], 3),
-        (ap + ["-e", "serial=0", "playbooks/upgrade-packages.yml"], 1),
-        (ap + ["sample.yml"], 1),
-        (["python2", "testing/healthchecks.py"], 2)
+        (["terraform", "plan", "--input=false", "--refresh=false"], 1),
     ]
-    debug = []
+    # TODO: replace this with either click or pypsi
+    if len(argv) < 2 or argv[1] != "plan-only":
+        setup.extend([
+            (["terraform", "apply"], 1),
+            (ap + ["playbooks/wait-for-hosts.yml"], 3),
+            (ap + ["-e", "serial=0", "playbooks/upgrade-packages.yml"], 1),
+            (ap + ["sample.yml"], 1),
+            (["python2", "testing/healthchecks.py"], 2)
+        ])
 
     if not run_cmds(setup, fail_sequential=True):
-        run_cmds(debug)
         exit(1)
-    # Travis works on the teardown, so there's no need to do so here.
--- testing/terraform/softlayer.tf
+++ testing/terraform/softlayer.tf
@@ -1,5 +1,8 @@
+variable build_number {}
 
 provider "softlayer" {
+  username = "test_username"
+  api_key = "test_api_key"
 }
 
 module "softlayer-keypair" {
@@ -11,10 +14,9 @@ module "softlayer-hosts" {
   source = "./terraform/softlayer/hosts"
   ssh_key = "${module.softlayer-keypair.keypair_id}"
 
-  hourly_billing = true
   region_name = "ams01"
-  domain = "example.com"
+  domain = "ci.mantl.io"
   control_count = 3
-  worker_count = 4
-  edge_count = 2
+  worker_count = 2
+  edge_count = 1
 }
