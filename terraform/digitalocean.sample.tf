300c3ba70167de5fbf3db02223d102a2f0ccfef0
Langston Barrett <langston.barrett@gmail.com>
2016-07-14 12:08:12 +0200

    clean up digitalocean terraform configs (#1682)
    * clean up digitalocean terraform configs
    
    * change short_name to name in testing/do.tf

--- terraform/digitalocean.sample.tf
+++ terraform/digitalocean.sample.tf
@@ -1,13 +1,18 @@
+# All of your resources will be prefixed by this name
+variable "name" { default = "mantl" }
+
 provider "digitalocean" {
   token = ""
 }
 
 module "do-keypair" {
+  name = "${var.name}"
   source = "./terraform/digitalocean/keypair"
   public_key_filename = "~/.ssh/id_rsa.pub"
 }
 
 module "do-hosts" {
+  name = "${var.name}"
   source = "./terraform/digitalocean/hosts"
   ssh_key = "${module.do-keypair.keypair_id}"
   region_name = "nyc3" # this must be a region with metadata support
