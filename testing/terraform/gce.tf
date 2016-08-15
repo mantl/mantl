2f3da6d1206014bb7dafc9c20d8dea59043882e4
Sam Hatfield <samuel.e.hatfield@gmail.com>
2016-08-15 14:21:42 -0500

    [TRAVIS] Remove DNS portion of testing GCE tf file
    Because our automated testing systems don't verify DNS setups, we need
    to remove the DNS module from the testing copy of the GCE terraform
    config file.

--- testing/terraform/gce.tf
+++ testing/terraform/gce.tf
@@ -107,20 +107,3 @@ module "network-lb" {
   instances = "${module.edge-nodes.instances}"
   short_name = "${var.short_name}"
 }
-
-module "cloud-dns" {
-  source = "./terraform/gce/dns"
-  control_count = "${var.control_count}"
-  control_ips = "${module.control-nodes.gce_ips}"
-  domain = "mydomain.com"
-  edge_count = "${var.edge_count}"
-  edge_ips = "${module.edge-nodes.gce_ips}"
-  lb_ip = "${module.network-lb.public_ip}"
-  managed_zone = "my-cloud-dns-zone"
-  short_name = "${var.short_name}"
-  subdomain = ".service"
-  worker_count = "${var.worker_count}"
-  worker_ips = "${module.worker-nodes.gce_ips}"
-  kubeworker_count = "${var.kubeworker_count}"
-  kubeworker_ips = "${module.kubeworker-nodes.gce_ips}"
-}
