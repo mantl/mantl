ac85a77a643436d141193be0ed21f517ae5c594d
Thomas Vincent <thomasvincent@gmail.com>
2017-06-05 01:40:08 -0700

    removing cisco reference

--- terraform/common/terraform-py/main.tf
+++ terraform/common/terraform-py/main.tf
@@ -1,7 +1,7 @@
 # Download terraform.py, a dynamic Ansible inventory
 resource "null_resource" "terraform-py" {
   provisioner "local-exec" {
-    command = "curl -sLo plugins/inventory/terraform.py https://raw.githubusercontent.com/CiscoCloud/terraform.py/master/terraform.py"
+    command = "curl -sLo plugins/inventory/terraform.py https://raw.githubusercontent.com/mantl/terraform.py/master/terraform.py"
   }
 
   provisioner "local-exec" {
